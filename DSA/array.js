c640a0df008c656a492e5fe045638a61da22a3e6
6ix9ine03 <magadan.laurence25@gmail.com>
2025-11-25 18:09:09 +0800

    added for-loop.js

--- DSA/array.js
+++ DSA/array.js
@@ -21,10 +21,47 @@ class Array{
         console.log(output);
     }
 
+ insert(index, element){
+        //Correcting Statement || Must be false
+        if(index <0 || index > this.length){
+            console.log("Invalid Index");
+            return;
+        }
+
+        //Shifting Condition
+        for(let i=this.length; i>index; i--){
+            this.data[i] = this.data[i-1];
+        }
+
+        //After Condition
+        this.data[index] = element;
+        this.length++;
+    }
 }
+ delete(index){
+
+        //Correcting Statement || Must be false
+        if(index <0 || index >= this.length){
+            console.log("Invalid Index");
+            return;
+        }
+
+        //Shifting Condition
+        for(let i= index; i< this.length; i++){
+            this.data[i] = this.data[i+1];
+        }
+
+        //After Condition
+        delete this.data[this.length-1];
+        this.length--;
+
+    }
 
 //
-
 arr1 = new Array([4,2,3]);
 
 arr1.traverse();
+arr1.insert(0,9);
+arr1.traverse();
+arr1.traverse();
+arr1.delete(1);
