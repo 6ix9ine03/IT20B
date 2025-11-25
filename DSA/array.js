1d40e9ca89025b1ccb58f0e2c5350fc8daef6c2e
6ix9ine03 <magadan.laurence25@gmail.com>
2025-11-25 18:47:55 +0800

    Added initial method for DSA/stack.js

--- DSA/array.js
+++ DSA/array.js
@@ -1,101 +1,33 @@
 
-class Array{
+class Stack{
 
-    constructor(initialData = []){
-        this.data = {};
-        this.length = 0;
+    constructor(){
+        this.data ={};
+        this.topIndex = -1;
+    }
 
-        //Assign element to index
-        for(let i=0; i<initialData.length; i++){
-            this.data[i] = initialData[i];
-            this.length++;
-        }
+    push(element){
+        this.topIndex++;
+        this.data[this.topIndex] = element;
     }
 
     traverse(){
         let output = "";
 
-        for(let i=0; i<this.length; i++){
+        for(let i=0; i<=this.topIndex; i++){
             output += this.data[i] + " ";
         }
 
         console.log(output);
     }
 
-    insert(index, element){
-        //Correcting Statement || Must be false
-        if(index <0 || index >= this.length){
-            console.log("Invalid Index");
-            return;
-        }
-
-        //Shifting Condition
-        for(let i=this.length; i>index; i--){
-            this.data[i] = this.data[i-1];
-        }
-
-        //After Condition
-        this.data[index] = element;
-        this.length++;
-    }
-
-    delete(index){
-
-        //Correcting Statement || Must be false
-        if(index <0 || index >= this.length){
-            console.log("Invalid Index");
-            return;
-        }
-
-        //Shifting Condition
-        for(let i= index; i< this.length; i++){
-            this.data[i] = this.data[i+1];
-        }
-
-        //After Condition
-        delete this.data[this.length-1];
-        this.length--;
-
-    }
-
-    update(index,element){
-        //Correcting Statement || Must be false
-        if(index <0 || index >= this.length){
-            console.log("Invalid Index");
-            return;
-        }
-
-        //After Condition
-        this.data[index] = element;
-    }
-
-    search(element){
-        //Must be true
-        for(let i=0; i< this.length;i++){
-            if(this.data[i] === element){
-                console.log(element + "  is found at index "+ i);
-                return i;
-            }
-        }
-
-        //False
-        console.log(element +" is not found at any index");
-        return -1;
-    }
+    
 }
 
 //
-arr1 = new Array([4,2,3]);
+stack1 = new Stack();
 
-arr1.traverse();
-arr1.insert(0,9);
-arr1.traverse();
-
-arr1.delete(1);
-arr1.traverse();
-
-arr1.update(2,5);
-arr1.traverse();
-
-arr1.search(43);
-arr1.search(9);
+stack1.push(4);
+stack1.push(2);
+stack1.push(3);
+stack1.traverse();
